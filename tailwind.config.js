b9ffcae7526511d46486b3b302c809db6f2abb50
unknown <gustiayumadeadiastuti@gmail.com>
2023-12-14 21:46:00 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'index.html',
+    "./src/**/*.{html,js}",
+    "./**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        dark: '#0f172a',
+      }
+    },
+  },
+  plugins: [],
+}
+
