83f4c5ca5e0c1e746763af1b7954545fc2946a51
unknown <gustiayumadeadiastuti@gmail.com>
2023-12-18 18:41:17 +0800

    pesan

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,20 @@ module.exports = {
     "./**/*.{html,js}"
   ],
   theme: {
+    container: {
+      center:true,
+      padding:'16px'
+    },
+    
     extend: {
       colors: {
         primary: '#14b8a6',
+        seconddary:'#64748b',
         dark: '#0f172a',
-      }
+      },
+      screens: {
+        '2xl': '1320px'
+      },
     },
   },
   plugins: [],
